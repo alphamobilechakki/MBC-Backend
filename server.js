f411cc4e346a19f5850b9035b78ce9693ea6d77b
Ankit <alphamobilechakki@gmail.com>
2025-09-26 15:38:11 +0530

    the port number is updated

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.use(express.json());
 app.use("/api", routes);
 
 // ✅ Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 const server = app.listen(PORT, () =>
   console.log(`🚀 Server running on port ${PORT}`)
 );
