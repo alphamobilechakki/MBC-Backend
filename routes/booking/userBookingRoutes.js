d5b77d0e75d43f3c9030bb679adf61f461f9671c
Ankit <alphamobilechakki@gmail.com>
2025-10-13 17:53:25 +0530

    userbooking updated

--- routes/booking/userBookingRoutes.js
+++ routes/booking/userBookingRoutes.js
@@ -4,16 +4,17 @@ import {
   cancelBookingByUser,
   getUserBookings,
 } from "../../controllers/booking/userBookingController.js";
+import authToken from "../../middleware/authToken.js"
 
 const router = express.Router();
 
 // ✅ Create booking (User)
-router.post("/create", createUserBooking);
+router.post("/create",authToken ,  createUserBooking);
 
 // ✅ Cancel booking (User)
-router.put("/cancel/:id", cancelBookingByUser);
+router.put("/cancel/:id", authToken ,  cancelBookingByUser);
 
 // ✅ Get user bookings
-router.get("/my-bookings/:userId", getUserBookings);
+router.get("/my-bookings/:userId",authToken ,  getUserBookings);
 
 export default router;
