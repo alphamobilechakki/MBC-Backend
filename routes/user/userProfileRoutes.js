0fbfbc22707a3e3a6801422f84805e8cb3ed17e5
Ankit <alphamobilechakki@gmail.com>
2025-10-17 16:24:24 +0530

    address updated

--- routes/user/userProfileRoutes.js
+++ routes/user/userProfileRoutes.js
@@ -6,6 +6,7 @@ import {
   deleteAddress,
   getDefaultAddress,
 } from '../../controllers/user/userProfileController.js';
+import { addAddress } from '../../controllers/user/addressController.js';
 import authToken from '../../middleware/authToken.js';
 
 const router = express.Router();
@@ -13,9 +14,11 @@ const router = express.Router();
 // User profile routes
 router.get('/profile', authToken, getUserProfile);
 router.put('/profile', authToken, updateUserProfile);
+router.post('/profile/address', authToken, addAddress);
 router.put('/profile/address/:addressId', authToken, updateAddress);
 router.delete('/profile/address/:addressId', authToken, deleteAddress);
 router.get('/profile/address/default', authToken, getDefaultAddress);
 
 
 export default router;
+
